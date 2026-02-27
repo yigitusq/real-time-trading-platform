654ee6d8d12c62324867a35c3b289620dc04ad72
yigitusq <yigituslu077@gmail.com>
2026-02-27 19:40:54 +0300

    initial commit

---
+++ src/main/java/com/yigitusq/market_data_service/MarketDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yigitusq.market_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yigitusq/market_data_service/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yigitusq.market_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
