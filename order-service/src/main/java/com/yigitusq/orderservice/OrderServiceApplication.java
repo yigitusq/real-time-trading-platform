77373215e078440a842fb02a48a578c0c0dd16b7
yigitusq <yigituslu077@gmail.com>
2026-03-12 16:56:15 +0300

    Redis implemented to the project, to PriceService.

--- order-service/src/main/java/com/yigitusq/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/yigitusq/orderservice/OrderServiceApplication.java
@@ -2,12 +2,13 @@ package com.yigitusq.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OrderServiceApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(OrderServiceApplication.class, args);
     }
-
 }
--- order-service/src/main/java/com/yigitusq/orderservice/service/PriceService.java
+++ order-service/src/main/java/com/yigitusq/orderservice/service/PriceService.java
@@ -1,19 +1,44 @@
 package com.yigitusq.orderservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
-import java.util.concurrent.ConcurrentHashMap;
+import java.time.Duration;
+// import java.util.concurrent.ConcurrentHashMap; // was using instead of redis
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class PriceService {
-    // Fiyatları RAM'de tutmak için
-    private final ConcurrentHashMap<String, BigDecimal> priceCache = new ConcurrentHashMap<>();
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String PRICE_KEY_PREFIX = "price:";
+    private static final Duration PRICE_TTL = Duration.ofSeconds(5);
 
     public void updatePrice(String symbol, BigDecimal price) {
-        priceCache.put(symbol, price);
+        try {
+            redisTemplate.opsForValue().set(
+                    PRICE_KEY_PREFIX + symbol,
+                    price.toPlainString(),
+                    PRICE_TTL
+            );
+        } catch (Exception e) {
+            log.warn("Redis'e fiyat yazılamadı: {} → {}", symbol, e.getMessage());
+        }
     }
 
     public BigDecimal getPrice(String symbol) {
-        return priceCache.getOrDefault(symbol, BigDecimal.ZERO);
+        try {
+            String price = redisTemplate.opsForValue()
+                    .get(PRICE_KEY_PREFIX + symbol.toUpperCase());
+            if (price != null) {
+                return new BigDecimal(price);
+            }
+        } catch (Exception e) {
+            log.warn("Redis'ten fiyat okunamadı: {} → {}", symbol, e.getMessage());
+        }
+        return BigDecimal.ZERO;
     }
 }
